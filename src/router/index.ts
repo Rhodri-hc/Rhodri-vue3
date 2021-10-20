bfe9bcd7da1a6bf72ae1d88018f0f2c1450c0628
Rhodri-hc <13622450494@163.com>
2021-10-20 17:50:19 +0800

    fix: 修改目录

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,16 @@ const routes: Array<RouteRecordRaw> = [
     name: 'Home',
     component: () => import(/* webpackChunkName: "Home" */ '@s/views/home/index')
   },
+  {
+    path: '/article',
+    name: 'Article',
+    component: () => import(/* webpackChunkName: "Home" */ '@s/views/article/index')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "Home" */ '@s/views/about/index')
+  },
   { path: '/', redirect: { name: 'Home' } }
 ]
 
