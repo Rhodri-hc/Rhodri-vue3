1a50dd66512b145e98f47fdfbb6ca629cdc5b1ff
Rhodri-hc <13622450494@163.com>
2021-10-21 23:07:26 +0800

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
+  <Header></Header>
   <router-view v-slot="{ Component }">
     <keep-alive>
       <component :is="Component" />
     </keep-alive>
   </router-view>
+  
 </template>
 
 <script lang="ts">
+
+import Header from "@s/components/header/index.vue"
 import { defineComponent } from "vue";
+
 export default defineComponent({
+  components:{
+    Header
+  },
   setup() {
     return {};
   },
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="bg-gray-100">
-      123131
-  </div>
+   <div className="relative pt-14">
+        <div className="w-11/12 sm:w-11/12 md:w-9/12 lg:w-8/12 xl:w-7/12 my-0 mx-auto">
+            <h3>文章</h3>
+
+            <div className="flex flex-col items-center mt-10 lg:mt-16">
+                <div className="flex rounded-lg overflow-hidden  h-48 lg:h-56  w-full  md:w-10/12 lg:w-9/12 shadow-lg  cursor-pointer transform hover:-translate-y-1 hover:scale-105 transition duration-700 ease-in-out">
+                    <img className="inset-0 w-1/2 h-full object-cover" alt="" src="@s/assets/images/qi.jpg" />
+                    <div className="p-6 md:p-8 text-xs lg:text-sm">
+                      <p className="text-sm sm:text-lg md:text-xl font-medium pb-4">让生活过得更好</p> 
+                      <p className="text-gray-500">一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好一定要让生活变得更好</p>
+                      <div className="flex items-center  justify-between  px-0 lg:px-2 pt-7"> 
+                          <span className="rounded-full py-0 lg:py-1  px-1 lg:px-2 border border-red-300 text-red-300">生活向</span>
+                          <span className="text-red-900">2021.03.15</span>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script lang="ts">
 import { defineComponent, reactive } from 'vue';
