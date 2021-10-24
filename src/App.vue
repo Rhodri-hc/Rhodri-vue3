3689969a2770d70bdad24642fdeb5fbb3f79a5cb
Rhodri-hc <13622450494@163.com>
2021-10-24 23:35:35 +0800

    fix:Feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
-  <Header></Header>
-  <router-view v-slot="{ Component }">
-    <keep-alive>
-      <component :is="Component" />
-    </keep-alive>
-  </router-view>
-  
+  <div className="font-sans flex flex-col justify-between h-screen">
+    <Header></Header>
+    <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+    <Footer className="mt-auto text-center flex-shrink-0"></Footer>
+  </div>
 </template>
 
 <script lang="ts">
 
 import Header from "@s/components/header/index.vue"
+import Footer from "@s/components/footer/index.vue"
 import { defineComponent } from "vue";
 
 export default defineComponent({
   components:{
-    Header
+    Header,
+    Footer
   },
   setup() {
     return {};
---
+++ src/components/footer/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div className="pt-15 pb-5 flex flex-col items-center text-sm opacity-75 ">
+        <div className="relative inline-flex justify-center">
+            <span className="pr-2">Crafted With</span>
+            <i className="createrHeart" /> 
+            <span className="pl-2">BY Rhodri</span>
+        </div>
+        <h5 className="pt-1 pl-4">2021.01 - {{getNowYearMonth()}} </h5>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue';
+
+
+export default defineComponent({
+  setup() {
+        // 获取当前年月
+    function getNowYearMonth(){
+        const nowDate = new Date();
+        const year = nowDate.getFullYear();
+        const month = nowDate.getMonth() + 1;
+        
+        
+        return month < 10 ? `${year}.0${month}` : `${year}.${month}`
+    }
+
+    return {
+        getNowYearMonth
+    }
+  },
+});
+</script>
+
+<style scoped >
+@import "./index.css";
+</style>
